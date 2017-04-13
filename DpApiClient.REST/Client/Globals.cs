c3ca8b779b99c10524d4c8a4bf7f4bc6dd9a747b
Ümit AKKAYA <umit_akkaya@live.nl>
2017-04-13 13:55:49 +0200

    Store auth token in memory

---
+++ DpApiClient.REST/Client/Globals.cs
@@ -0,0 +1,57 @@
+﻿using DpApiClient.REST.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DpApiClient.REST.Client
+{
+    public sealed class Globals
+    {
+
+        private static object locker = new object();
+        private static Dictionary<string, AuthorizationToken> _tokenStorage = new Dictionary<string, AuthorizationToken>();
+        public static Dictionary<string, AuthorizationToken> TokenStorage
+        {
+            get
+            {
+                lock (locker)
+                {
+                    return _tokenStorage;
+                }
+            }
+            internal set
+            {
+                lock (locker)
+                {
+                    _tokenStorage = value;
+                }
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public static AuthorizationToken GetToken(string clientId)
+        {
+            AuthorizationToken token = null;
+            _tokenStorage.TryGetValue(clientId, out token);
+            return token;
+        }
+
+        public static void SetToken(string clientId, AuthorizationToken token)
+        {
+            if (_tokenStorage.ContainsKey(clientId))
+            {
+                _tokenStorage[clientId] = token;
+            }
+            else
+            {
+                _tokenStorage.Add(clientId, token);
+            }
+        }
+
+    }
+}
